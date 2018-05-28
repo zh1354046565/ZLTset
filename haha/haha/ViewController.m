d2427c4590bc6b47a5d12651bb34047ff9beb551
卡羞羞 <15002477524@163.com>
2018-05-28 14:43:54 +0800

    345

--- haha/haha/ViewController.m
+++ haha/haha/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+    self.navigationItem.title = @"哟哟哟";
+    
 }
 
 
